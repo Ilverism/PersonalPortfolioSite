28baa9b16d953f0a0f5079f038ac4aae99c7955f
Ilverism <72947008+Ilverism@users.noreply.github.com>
2024-03-17 04:20:09 -0400

    +Horizontal Buttons, 3 Button Links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ShaderBackgroundComponent } from './shader-background/shader-background.component';
+import { HorizontalButtonComponent } from './horizontal-button/horizontal-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ShaderBackgroundComponent],
+  imports: [CommonModule, RouterOutlet, ShaderBackgroundComponent, HorizontalButtonComponent],
   templateUrl: './app.component.html',
-  //templateUrl: './shader-background/shader-background.component.html',
-  //template: '<app-shader-background></app-shader-background>',
   styleUrl: './app.component.css'
 })
-export class AppComponent { }
+export class AppComponent {
+
+  title = 'PersonalPortfolioSite';
+
+  onClickLinkedIn(event: Event) {
+    window.open('https://www.linkedin.com/in/aidan-cusack-74406728b/', '_blank');
+    }
+  onClickGitHub(event: Event) {
+    window.open('https://github.com/Ilverism/', '_blank');
+    }
+  onClickYouTube(event: Event) {
+    window.open('https://www.youtube.com/@IlverismOST/', '_blank');
+    }  
+
+}
--- src/app/horizontal-button/horizontal-button.component.ts
+++ src/app/horizontal-button/horizontal-button.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-horizontal-button',
   standalone: true,
-  imports: [],
+  selector: 'app-horizontal-button',
   templateUrl: './horizontal-button.component.html',
   styleUrl: './horizontal-button.component.css'
 })
+
+
 export class HorizontalButtonComponent {
 
-}
+  @Input() buttonText:string = "Button";
+  @Input() buttonColor:string = "blue";
+  @Input() buttonOpacity:number = 1.00;
+  @Input() iconClass:string = "";
+  @Output() onClick:Function = () => {};
+
+  handleClick(event:Event):void {
+    this.onClick(event);
+    }
+
+  }
