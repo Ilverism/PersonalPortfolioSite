07320c24b6e62687e10c5b028242998c0ead3198
Ilverism <72947008+Ilverism@users.noreply.github.com>
2024-04-12 16:37:59 -0400

    Improve scrollbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component, Directive, HostListener } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ShaderBackgroundComponent } from './shader-background/shader-background.component';
 import { HorizontalButtonComponent } from './horizontal-button/horizontal-button.component';
 import { TabComponent } from './tab/tab.component';
+import { NgScrollbarModule } from 'ngx-scrollbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ShaderBackgroundComponent, HorizontalButtonComponent, TabComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ShaderBackgroundComponent,
+    HorizontalButtonComponent,
+    TabComponent,
+    NgScrollbarModule
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
